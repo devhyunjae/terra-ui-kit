fe39f11e7da771ca61e7b3319570a58dbb3c41aa
Victor <victor@terra.money>
2021-10-20 11:52:19 +0900

    Add Button component

--- src/@terra-ui-kit/station-ui/buttons/Button.tsx
+++ src/@terra-ui-kit/station-ui/buttons/Button.tsx
@@ -4,21 +4,91 @@ import {
 } from '@terra-ui-kit/base-components';
 import { fixHMR } from 'fix-hmr';
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { Size, Variant } from '../theme/entities';
 
-export interface ButtonProps extends Omit<UnstyledButtonProps, 'ref'> {}
+export interface ButtonProps extends Omit<UnstyledButtonProps, 'ref'> {
+  variant: Variant;
+  size: Size;
+  fullWidth?: boolean;
+}
 
 function ButtonBase(buttonProps: ButtonProps) {
   return <UnstyledButton {...buttonProps} />;
 }
 
+const variantStyles = {
+  primary: css`
+    background-color: var(--color-primary300);
+    color: var(--color-gray100);
+  `,
+  danger: css`
+    background-color: var(--color-danger01);
+    color: var(--color-gray100);
+  `,
+  outline: css`
+    background-color: transparent;
+    color: var(--color-primary400);
+    border: 1px solid var(--color-primary400);
+    &:hover {
+      filter: none;
+      font-weight: 700;
+    }
+  `,
+  default: css`
+    background-color: var(--color-desaturated300);
+    color: var(--color-primary400);
+  `,
+};
+
+const sizeStyles = {
+  large: css`
+    padding: 18px 20px;
+  `,
+  medium: css`
+    min-width: 100px;
+    padding: 10px 20px;
+  `,
+  small: css`
+    min-width: 100px;
+    padding: 5px 20px;
+  `,
+  default: css``,
+};
+
 const _Button = styled(ButtonBase)`
-  background-color: var(--color-gray18);
-  color: var(--color-white80);
-
-  padding: var(--button-padding);
-
-  font-size: 1em;
+  border: 0;
+  outline: 0;
+  font-family: inherit;
+  font-size: inherit;
+  font-weight: inherit;
+  line-height: inherit;
+  cursor: pointer;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 1.5;
+  min-width: 150px;
+  padding: 13px 20px;
+  border-radius: 24px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &:hover {
+    filter: brightness(0.95);
+  }
+  ${({ variant = 'default' }) => variantStyles[variant]}
+  ${({ size = 'default' }) => sizeStyles[size]}
+  width: ${({ fullWidth = false }) => (fullWidth ? '100%' : 'auto')};
+  ${({ disabled = false }) =>
+    disabled
+      ? css`
+          opacity: 0.3;
+          cursor: not-allowed;
+          &:hover {
+            filter: none;
+          }
+        `
+      : null};
 `;
 
 export const Button = fixHMR(_Button);
---
+++ src/@terra-ui-kit/station-ui/theme/entities.ts
@@ -0,0 +1,2 @@
+export type Variant = 'default' | 'primary' | 'danger' | 'outline';
+export type Size = 'default' | 'large' | 'medium' | 'small';
