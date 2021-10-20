28a401dddfd989538271b32966077ee317a272b7
Victor <victor@terra.money>
2021-10-20 13:25:40 +0900

    Fix ButtonProps to be optional

--- src/@terra-ui-kit/station-ui/buttons/__stories__/Button.stories.tsx
+++ src/@terra-ui-kit/station-ui/buttons/__stories__/Button.stories.tsx
@@ -5,25 +5,27 @@ export default {
   title: 'station-ui/Button',
 };
 
-export const Basic = () => (
-  <>
-    <div
-      style={{
-        display: 'flex',
-        gap: 20,
-        marginBottom: 20,
-      }}
-    >
-      <Button>default</Button>
-      <Button variant="primary">primary</Button>
-      <Button variant="danger">danger</Button>
-      <Button variant="outline">outline</Button>
-    </div>
-    <Button fullWidth>fullWidth</Button>
-    <Button size="large">large</Button>
-    <Button>default size</Button>
-    <Button size="medium">medium</Button>
-    <Button size="small">outline</Button>
-    <Button disabled>Disabled</Button>
-  </>
-);
+export const Basic = () => {
+  return (
+    <>
+      <div
+        style={{
+          display: 'flex',
+          gap: 20,
+          marginBottom: 20,
+        }}
+      >
+        <Button>default</Button>
+        <Button variant="primary">primary</Button>
+        <Button variant="danger">danger</Button>
+        <Button variant="outline">outline</Button>
+      </div>
+      <Button fullWidth>fullWidth</Button>
+      <Button size="large">large</Button>
+      <Button>default size</Button>
+      <Button size="medium">medium</Button>
+      <Button size="small">outline</Button>
+      <Button disabled>Disabled</Button>
+    </>
+  );
+};
