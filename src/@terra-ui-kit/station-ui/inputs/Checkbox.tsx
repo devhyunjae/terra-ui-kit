aaed83910639439ecd5018354fea9973be93d781
Victor <victor@terra.money>
2021-10-26 17:01:13 +0900

    Add blue color for Checkbox component

--- src/@terra-ui-kit/station-ui/inputs/Checkbox.tsx
+++ src/@terra-ui-kit/station-ui/inputs/Checkbox.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+type ColorType = 'default' | 'blue';
 
 interface Props {
   checked: boolean;
   onClick: () => void;
   label?: string;
   className?: string;
+  color?: ColorType;
 }
 
-const Checkbox = ({ checked, onClick, label, className }: Props) => {
+interface ColorProps {
+  color: ColorType;
+}
+
+const Checkbox = ({
+  checked,
+  onClick,
+  label,
+  className,
+  color = 'default',
+}: Props) => {
   return (
     <Container onClick={onClick} className={className}>
-      <CheckboxContainer>{checked && <Checked />}</CheckboxContainer>
+      <CheckboxContainer color={color}>
+        {checked && <Checked color={color} />}
+      </CheckboxContainer>
       {label && <Label>{label}</Label>}
     </Container>
   );
@@ -23,7 +38,7 @@ const Container = styled('div')`
   width: fit-content;
 `;
 
-const CheckboxContainer = styled('div')`
+const CheckboxContainer = styled('div')<ColorProps>`
   cursor: pointer;
   flex-shrink: 0;
   display: flex;
@@ -33,13 +48,24 @@ const CheckboxContainer = styled('div')`
   height: 16px;
   border-radius: 2px;
   border: solid 1px var(--color-desaturated400);
+  ${({ color }) =>
+    color === 'blue' &&
+    css`
+      border: solid 1px var(--color-desaturated800);
+      background-color: var(--color-primary400);
+    `}
 `;
 
-const Checked = styled('div')`
+const Checked = styled('div')<ColorProps>`
   width: 8px;
   height: 8px;
   border-radius: 2px;
   background-color: var(--color-primary400);
+  ${({ color }) =>
+    color === 'blue' &&
+    css`
+      background-color: var(--color-white);
+    `}
 `;
 
 const Label = styled('p')`
--- src/@terra-ui-kit/station-ui/inputs/__stories__/Checkbox.stories.tsx
+++ src/@terra-ui-kit/station-ui/inputs/__stories__/Checkbox.stories.tsx
@@ -8,6 +8,7 @@ export default {
 export const Basic = () => {
   const [checked, setChecked] = useState(false);
   const [anotherChecked, setAnotherChecked] = useState(false);
+  const [blueChecked, setBlueChecked] = useState(false);
   return (
     <>
       Only checkbox
@@ -28,6 +29,14 @@ export const Basic = () => {
           label="I have securely WRITTEN DOWN MY SEED. I understand that lost seeds cannot be recovered."
         />
       </div>
+      <div style={{ margin: 20 }} />
+      <div>blue checkbox with label</div>
+      <Checkbox
+        color="blue"
+        checked={blueChecked}
+        onClick={() => setBlueChecked(!blueChecked)}
+        label="I have securely WRITTEN DOWN MY SEED. I understand that lost seeds cannot be recovered."
+      />
     </>
   );
 };
